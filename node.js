63b7a8b0a311ffb0a46138c04d7f1a9446c4a92e
Miura55 <acordion.piano@gmail.com>
2020-11-29 02:38:43 +0900

    Fig logging

--- node.js
+++ node.js
@@ -1,17 +1,14 @@
-module.exports = function(RED) {
-    function LowerCaseNode(config) {
-        RED.nodes.createNode(this,config);
-        this.inputText = config.inputText;
-        var node = this;
-        node.on('input', function(msg) {
-            msg.payload = msg.payload.toLowerCase();
-            // logging
-            var log4js = require('log4js');
-            var log = log4js.getLogger();
-            log.level = 'debug';
-            log.debug(node.inputText);
-            node.send(msg);
-        });
-    }
-    RED.nodes.registerType("lower-case",LowerCaseNode);
-}
+module.exports = function (RED) {
+  function LowerCaseNode(config) {
+    RED.nodes.createNode(this, config);
+    this.inputText = config.inputText;
+    var node = this;
+    node.on("input", function (msg) {
+      msg.payload = msg.payload.toLowerCase();
+      // logging
+      RED.log.info(msg.payload);
+      node.send(msg);
+    });
+  }
+  RED.nodes.registerType("lower-case", LowerCaseNode);
+};
